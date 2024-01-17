3dd8be1171b209ddf18f4ab902a5e06ed7070b38
abhilashvc777 <145086618+abhilashvc777@users.noreply.github.com>
2024-01-17 13:22:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-//your JS code here. If required.
+// Object called student with a property called name
+const student = {
+    name: "John Doe"
+};
+
+// Adding a method to the Object prototype
+Object.prototype.getKeys = function() {
+    // Using Object.keys to get an array of keys
+    return Object.keys(this);
+};
+
+// Example usage
+console.log(student.getKeys()); // Output: ['name']
+
